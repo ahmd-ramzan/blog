7d8f54620dbb4cfd1afd968fccd5370b2e644534
ahmd-ramzan <iamahmdrmzn@gmail.com>
2022-11-19 22:06:17 +0500

    pinia for state managment

--- blog-client/src/main.js
+++ blog-client/src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import './assets/app.css'
 import router from './router'
 import axios from 'axios'
+import {createPinia} from "pinia"
+const pinia = createPinia()
 
 import App from './App.vue'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
 axios.defaults.withCredentials = true
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(pinia).mount('#app')
--- blog-client/src/pages/Home.vue
+++ blog-client/src/pages/Home.vue
@@ -13,10 +13,13 @@
 
 <script>
 import { onMounted } from 'vue'
-import usePosts from "../api/usePosts";
+import usePosts from "../api/usePosts"
+import {useAppStore} from "../store/app"
 
 export default {
   setup() {
+    const appStore = useAppStore()
+    console.log(appStore)
     const { posts, fetchPosts } = usePosts()
 
     onMounted(fetchPosts)
