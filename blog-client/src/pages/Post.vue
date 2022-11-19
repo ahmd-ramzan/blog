4e8dcc6b473a371cff1eed4fe12f83d7cb847304
ahmd-ramzan <iamahmdrmzn@gmail.com>
2022-11-19 16:33:29 +0500

    fetch-single-post

---
+++ blog-client/src/pages/Post.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1 class="block text-center text-4xl sm:text-6xl leading-10 font-extrabold tracking-tight text-gray-900">
+      {{ post.title }}
+    </h1>
+    <div v-html="post.body" class="mt-16 text-gray-500 prose prose-sm lg:prose"></div>
+  </div>
+</template>
+
+<script>
+import usePosts from "../api/usePosts"
+import { onMounted } from 'vue'
+export default {
+  props: {
+    slug: {
+      required: true,
+      type: String
+    }
+  },
+  setup(props) {
+    const { post, fetchPost } = usePosts()
+
+    onMounted(() => fetchPost(props.slug))
+
+    return {
+      post,
+      fetchPost
+    }
+  }
+}
+</script>
--- blog-client/src/router/index.js
+++ blog-client/src/router/index.js
@@ -1,9 +1,11 @@
 import {createRouter, createWebHistory} from 'vue-router'
 
 import Home from '../pages/Home.vue'
+import Post from '../pages/Post.vue'
 
 const routes = [
-    {path: '/', name: 'home', component: Home}
+    {path: '/', name: 'home', component: Home},
+    {path: '/posts/:slug', name:'post', component: Post, props: true}
 ]
 
 export default createRouter({
