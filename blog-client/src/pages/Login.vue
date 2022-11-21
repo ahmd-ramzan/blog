b2e19f3ceb3c711aae9df74b01488514c8b47326
ahmd-ramzan <iamahmdrmzn@gmail.com>
2022-11-21 11:59:15 +0500

    creating login page

--- blog-client/src/pages/Login.vue
+++ blog-client/src/pages/Login.vue
@@ -1,25 +1,46 @@
 <template>
-  <div>
-    <form v-on:submit.prevent="login">
-      <button type="submit">Login</button>
+  <div class="max-w-md mx-auto w-full">
+    <form v-on:submit.prevent="attemptLogin" class="mt-8 space-y-6">
+      <div class="rounded-md shadow-sm space-y-4">
+        <div>
+          <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+          <div class="mt-1">
+            <input v-model="form.email" type="text" name="email" id="email" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="you@example.com">
+          </div>
+        </div>
+        <div>
+          <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+          <div class="mt-1">
+            <input v-model="form.password" type="password" name="password" id="password" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
+          </div>
+        </div>
+      </div>
+
+      <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+        Sign in
+      </button>
     </form>
   </div>
 </template>
 
 <script>
 import {useAppStore} from "../store/app";
+import { reactive } from 'vue'
 
 export default {
   setup () {
     const appStore = useAppStore()
-    const login = () => {
-      appStore.login({
-        email: 'leuschke.janet@example.org',
-        password: 'password'
-      })
+
+    const form = reactive({
+      email: '',
+      password: ''
+    })
+    const attemptLogin = () => {
+      appStore.login(form)
     }
     return {
-      login
+      attemptLogin,
+      form
     }
   }
 }
